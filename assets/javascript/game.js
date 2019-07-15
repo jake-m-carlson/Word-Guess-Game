3da869e78f5ff56537181ff52f96765e10ca5a3f
Jake Carlson <jakecarlson@Jakes-MBP.hsd1.mn.comcast.net>
2019-07-15 15:44:13 -0500

    linked javascript to html, wrote variables for words & letters

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+var words = ["baloo" , "simba" , "thumper" , "pegasus" , "flounder" , "rajah" , "winnie"];
+
+for (var i=0; i < words.length; i++) {
+    // console.log(words[i]); ----- works!
+}
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+for (var i=0; i < letters.length; i++) {
+    // console.log(letters[i]); ------- works!
+}
